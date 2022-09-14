561cc277171758e539717d46d15433665691c487
Prateek7401 <prateek.richhariya_cs19@gmail.com>
2022-09-14 17:54:19 +0530

    first commit

---
+++ src/Game.ts
@@ -0,0 +1,40 @@
+import {Application, Sprite} from 'pixi.js';
+import {gsap} from 'gsap';
+export class Game extends Application{
+    private spin:boolean;
+    private sliceAngle = 360/7;
+    constructor(opts:any) {
+        super(opts);
+        this.preload([
+            {name:'wheel', url:'assets/wheel.webp'},
+            {name:'arrow',url:'assets/arrow1.png'}
+        ], this.onLoad.bind(this));
+    }
+    preload(list:any[], cb:()=>{}):void {
+        this.loader.add(list);
+        this.loader.load(cb);
+    }
+    onLoad():void {
+        const wheel = new Sprite(this.loader.resources['wheel'].texture);
+        const arrow=new Sprite(this.loader.resources['arrow'].texture);
+        wheel.anchor.set(0.5);
+        arrow.anchor.set(0.5);
+        wheel.x = this.screen.width/2;
+        wheel.y = this.screen.height/2;
+        arrow.x = this.screen.width/2;
+        arrow.y = this.screen.height/2-250;
+        arrow.width=50;
+        arrow.height=65;
+        this.stage.addChild(wheel);
+        this.stage.addChild(arrow);
+
+        wheel.interactive = true;
+        wheel.buttonMode = true;
+        console.log(this.stage);
+        wheel.on('pointerup', ()=>{
+            let random = Math.floor(Math.random()*7);
+            let stopAngle = random * this.sliceAngle;
+            gsap.fromTo(wheel,{angle:0},{angle:3600+stopAngle, duration:5, ease:'expo.out'});
+        });
+    }
+}
